859cfb9faa21e90a35269aa9541077dd415b65db
hussainkor <hussainkor@gmail.com>
2024-10-27 15:19:57 +0530

    first commit for auto review scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { reviewData } from './data';
+import { quoteIcon, nextIcon, prevIcon } from './Icons';
 
 function App() {
+  const [peoples, setPeoples] = useState(reviewData);
+  const [index, setIndex] = useState(0);
+
+  const handlePrev = () => {
+    index === 0 ? setIndex(peoples.length - 1) : setIndex((p) => p - 1)
+  }
+  const handleNext = () => {
+    index === peoples.length - 1 ? setIndex(0) : setIndex((n) => n + 1)
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      index === peoples.length - 1 ? setIndex(0) : setIndex(index + 1)
+    }, 2000)
+    return () => clearInterval(slider)
+  }, [index])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className='title'>
+        <h2><span>{index + 1}/</span>Reviews</h2>
+      </div>
+      <div className='section-center'>
+        {peoples.map((people, peopleIndex) => {
+          const { id, title, image, name, quote } = people;
+          let position = 'nextSlide';
+          if (peopleIndex === index) {
+            position = "activeSlide"
+          }
+          if (peopleIndex === index - 1 || (index === 0 && peopleIndex === people.length - 1)) {
+            position = "lastSlide"
+          }
+          return <article key={id} className={position}>
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <h4>{title}</h4>
+            <p>{quote}</p>
+            {quoteIcon}
+          </article>
+        })}
+      </div>
+      <div className='button-control'>
+        <button onClick={handlePrev} className='handle-btn'>{prevIcon}</button>
+        <button onClick={handleNext} className='handle-btn'>{nextIcon}</button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const reviewData = [
+    {
+        id: 1,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSi8EO26WdA-KDg_Hn5Wx2X2FNAOxMeycBvhgmX5lTG7yJi_vgzMPM52cpPKwOE6sPDJO8&usqp=CAU",
+        name: 'Shahzad Karim',
+        title: 'Interior Designer',
+        quote: 'Overall Good Looks and Bold look Running like On Road ossum and interior is also good not a average but good and overall milage is also Better and in CNG ossum'
+    },
+    {
+        id: 2,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxapDwCeVLL0T69nhwV_BgqH9lztNDYQGhCbUVKHMgITKzGDlPsa55HS-6dqUdC8Qt5VU&usqp=CAU",
+        name: 'Raghib Hussain',
+        title: 'Frontend Developer',
+        quote: 'Average is very low from company claims 17.5only. Style and look is good but note safe. Risk is your'
+    },
+    {
+        id: 3,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbvHpEOo3fTsZQkp5Hfd93YrByM9P4F944GylcXSwZ7xzop2YZo6KH7lFdu1FsdBJ3E8g&usqp=CAU",
+        name: 'Asif Iqbal',
+        title: 'Civil Engineer',
+        quote: 'Cost effective and reliable engine makes Maruti a default choice in Indian households. Maruti has worked on its body issues and brought back the trust we have with Maruti cars with Fronx.'
+    },
+    {
+        id: 4,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpQoIKo3El-UKkcxy8uS-arKOsM0tbI3yMZuI_DnDLL37tdBU2sPtNTSsD8pMU0i2-DOE&usqp=CAU",
+        name: 'Raees Azam',
+        title: 'Software Engineer',
+        quote: 'Mileage, features, comforts aree likee good butt itz bold and starring design are Just killed itt highly recommend for family because space are nice inside thiss according to me bestt car from maruti'
+    },
+    {
+        id: 5,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTcUnv77tNZ4m9l8OvOwnDLheOSWMsrayF4rhvApkyoOF2SI9m3kAoDrx1rDCz9DAH1Lg&usqp=CAU",
+        name: 'Zaheer Khan',
+        title: 'Cricketer',
+        quote: 'Family car or you can say a perfect suv for boys also nice interiors great feeling U cn say perfect car or suv for future . Thanks to maruti'
+    },
+    {
+        id: 6,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_pgbZQKfFX10RBv-IDDzM9Ety2UpwVp1igauCI1o63eVAOtdvPcf6Ty5XcJq78hc88dk&usqp=CAU",
+        name: 'Shadab Azam',
+        title: 'Architecture',
+        quote: 'Fronx is the ride of personalities, It gives a new way to pass the congestion. Its comfortness is out of mind.it bold look Different you from your colleagues.Its bold and brave in look, and mileage wow'
+    },
+    {
+        id: 7,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwYZeZ2LduOmysSwgiQSnETsu_e82DkgEPI8HOghsnd3DLUDLbWe4od5a-6FN6otW8mjY&usqp=CAU",
+        name: 'Maher Fatma',
+        title: 'CA',
+        quote: 'Value for money ,great design and looks is very nice ,fuel efficiency is very good awesome it has best car for family and you can use in buisness as well.'
+    },
+]
